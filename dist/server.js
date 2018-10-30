4e01f5df45b8750c37363b8a4433818817cbece4
lucaciotti <luca.ciotti@gmail.com>
2018-10-30 10:48:34 +0100

    Keep trying heroku

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = process.env.PORT || 5000;
-app_1.default.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+let port = process.env.PORT || 5000;
+app_1.default.listen(port, () => {
+    console.log('Express server listening on port ${port}');
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
-const PORT = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log('Express server listening on port ' + PORT);
+app.listen(port, () => {
+    console.log('Express server listening on port ${port}');
 })
